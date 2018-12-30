348f10d044d4c72080bbd4ff7c79dc1195943d5d
Dan Olson <dano7900@gmail.com>
2018-12-30 19:24:23 +0000

    initial setup

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,6 +2,13 @@
 
 void ofApp::setup(){
 
+send = false;
+
+serial.listDevices();
+vector <ofSerialDeviceInfo> deviceList = serial.getDeviceList();
+
+int baud_rate = 115200;
+
 }
 
 void ofApp::update(){
